243106515d56c16b14f5a4dd9e4ba02704549d41
pswai <parksoon_my@hotmail.com>
2021-10-04 20:50:46 +0800

    [Ex06] Hierarchical state

--- src/06/timerMachine.js
+++ src/06/timerMachine.js
@@ -1,52 +1,73 @@
-import { createMachine, assign } from 'xstate';
+import { createMachine, assign } from "xstate";
 
 const timerExpired = (ctx) => ctx.elapsed >= ctx.duration;
 
-export const timerMachine = createMachine({
-  initial: 'idle',
-  context: {
-    duration: 5,
-    elapsed: 0,
-    interval: 0.1,
-  },
-  states: {
-    idle: {
-      entry: assign({
-        duration: 5,
-        elapsed: 0,
-      }),
-      on: {
-        TOGGLE: 'running',
-      },
+export const timerMachine = createMachine(
+  {
+    initial: "idle",
+    context: {
+      duration: 5,
+      elapsed: 0,
+      interval: 0.1,
     },
-    running: {
-      // Add the `normal` and `overtime` nested states here.
-      // Don't forget to add the initial state (`normal`)!
-      // ...
+    states: {
+      idle: {
+        entry: assign({
+          duration: 5,
+          elapsed: 0,
+        }),
+        on: {
+          TOGGLE: "running",
+        },
+      },
+      running: {
+        // Add the `normal` and `overtime` nested states here.
+        // Don't forget to add the initial state (`normal`)!
+        initial: "normal",
+        states: {
+          normal: {
+            always: {
+              cond: "timerExpired",
+              target: "overtime",
+            },
+            on: {
+              RESET: undefined,
+            },
+          },
+          overtime: {
+            on: {
+              TOGGLE: undefined,
+            },
+          },
+        },
 
-      on: {
-        TICK: {
-          actions: assign({
-            elapsed: (ctx) => ctx.elapsed + ctx.interval,
-          }),
+        on: {
+          TICK: {
+            actions: assign({
+              elapsed: (ctx) => ctx.elapsed + ctx.interval,
+            }),
+          },
+          TOGGLE: "paused",
+          ADD_MINUTE: {
+            actions: assign({
+              duration: (ctx) => ctx.duration + 60,
+            }),
+          },
         },
-        TOGGLE: 'paused',
-        ADD_MINUTE: {
-          actions: assign({
-            duration: (ctx) => ctx.duration + 60,
-          }),
+      },
+      paused: {
+        on: {
+          TOGGLE: "running",
         },
       },
     },
-    paused: {
-      on: {
-        TOGGLE: 'running',
+    on: {
+      RESET: {
+        target: ".idle",
       },
     },
   },
-  on: {
-    RESET: {
-      target: '.idle',
-    },
-  },
-});
+  {
+    guards: { timerExpired },
+  }
+);
